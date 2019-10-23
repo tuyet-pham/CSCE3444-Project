975702ec9dbcdd6faf266e867ff7fde5f66d8378
tuyet-pham <tiipham19@gmail.com>
2019-10-23 17:58:31 -0500

    Comments

--- .py/mysqlsetup.py
+++ .py/mysqlsetup.py
@@ -1,21 +1,42 @@
+#!usr/bin/env python
+# mysql.connector & it's corresponding error code for to be able to connect to the mysql database
+#
+# The following code is to 'insert into' the scraped scholarships ONLY. 
+# This script is not used for the scholarships that are pending for removal or acceptance.
+#
+# The following packages :
+#   $ pip install MySQL-python
+#   $ pip install mysql-connector-python
+
 import mysql.connector as sql
 import sys, os
 from mysql.connector import errorcode
 import csv 
 from datetime import date
 
+
+
 try:
+    # Try the connection : user=root pass=password1 host='localhsot' or host='127.0.0.1' database='scholarscrape'
+    # Make a named cursor. This will allow us to execute the commands. Acts as the ( > ) like in terminal.
     conx = sql.connect(user=sys.argv[1], password=sys.argv[2], host='127.0.0.1', database='scholarscrape')
     cursor = conx.cursor()
+
+    # This is to look for the .csv file that was scanned the same day this script willrun and store the data.
     time=date.today()
 
     filename='scan_'+str(time)+'.csv'
     reader = csv.reader(open(filename))
 
+    # for each row in the csv file the cursor will make a relation in the table `Scholarship` with name, URL, amount and deadline.
     for row in reader:
-        cursor.execute("INSERT INTO Scholarship (name, URL, amount, deadline) VALUES (%s,%s,%s,%s)", row)
+        cursor.execute("INSERT INTO Scholarship (name, url, amount, deadline, ) VALUES (%s,%s,%s,%s)", row)
+        
+
+    # Close when done. 
     conx.commit()
     cursor.close()
+    conx.close()
     print("done") 
 
 except sql.Error as er:
@@ -25,5 +46,6 @@ except sql.Error as er:
         print("Database does not exit")
     else:
         print(er)
+
 
        
