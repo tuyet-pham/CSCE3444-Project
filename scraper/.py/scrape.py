ac148df169503b40b31ed18794ba656ac0583d0c
tuyet-pham <tiipham19@gmail.com>
2019-10-30 18:40:29 -0500

    "Moved .py and made into function"

---
+++ scraper/.py/scrape.py
@@ -0,0 +1,45 @@
+#!usr/bin/env python
+# mysql.connector & it's corresponding error code for to be able to connect to the mysql database
+#
+# The following code is to 'insert into' the scraped scholarships ONLY. 
+# This script is not used for the scholarships that are pending for removal or acceptance.
+#
+# The following packages :
+#   $ pip install MySQL-python
+#   $ pip install mysql-connector-python
+
+import mysql.connector as sql
+import sys, os
+from mysql.connector import errorcode
+import csv 
+from datetime import date
+
+def scrape(usern, passwd, hostl, databasen):
+    try:
+        conx = sql.connect(user=usern, password=passwd , host=hostl, database=databasen)
+        cursor = conx.cursor()
+
+        time=date.today()
+        filename='scan_'+str(time)+'.csv'
+        reader = csv.reader(open(filename))
+
+        # for each row in the csv file the cursor will make a relation in the table `Scholarship` with name, URL, amount and deadline.
+        for row in reader:
+            cursor.execute("INSERT INTO Scholarship (name, url, amount, deadline ) VALUES (%s,%s,%s,%s)", row)
+
+
+
+        # Close 
+        conx.commit()
+        cursor.close()
+        conx.close()
+    except sql.Error as er:
+        if er.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with username or password")
+        elif er.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exit")
+        else:
+            print(er)
+
+
+        
