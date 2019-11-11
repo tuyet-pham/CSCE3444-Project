ef63a1d8a72be649ca6a0c81dc391ddf9ebf4593
tuyet-pham <tiipham19@gmail.com>
2019-11-10 19:59:01 -0600

    Rmoved unnecessary files

--- flask/sqlScript.py
+++ flask/sqlScript.py
@@ -2,11 +2,12 @@ insertQuery="""
     INSERT INTO Scholarship (name, url, amount, deadline, accp_status ) VALUES (%s,%s,%s,%s,%s)
     """
 
-feature="""
-AND %s = %s;
-"""
 
 select="""
     SELECT * FROM Scholarship WHERE %s = %s;
     """
+
+feature="""
+AND %s = %s;
+"""
 
--- flask/template/readme.txt
+++
@@ -1,1 +0,0 @@
-Put your html files here. :)
