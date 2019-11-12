e117451e5562cf787bdd77786fa8445a7f882867
tuyet-pham <tiipham19@gmail.com>
2019-11-12 13:15:13 -0600

    fixed

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,143 +1,156 @@
-#!/usr/bin/python
-"""This is an HTML scraper that does the following.
-1. Go to https://www.scholarships.com/financial-aid/college-scholarships/scholarship-directory/academic-major/computer-science
-2. Scrape the first page of listings (<td> elements inside <tr>)
-3. Output data as importable CSB (Outdated step: Turn the collected data into JSON)
-4. Store the JSON to file, where it can be accessed by the Py-SQL script
-"""
-# scholscrape_new.py -- optimized for Python v3.7.3.
+#!usr/bin/env python3
+# mysql.connector & it's corresponding error code for to be able to connect to the mysql database
+#
+# This script is not used for the scholarships that are pending for removal or acceptance.
+#
+# This file also has functions : Please scroll for more into
+#   (1). def toAmount(str) 
+#   (2). def toDate(str)
+#   (3). def tagBuilder(str, str, str)
+#
+# The following packages:
+#   $ pip3 install mysql-connector-python
 
-# CONTRIBUTORS: Peyton Pritchard, Joe Maggio
+import mysql.connector as sql
+import sys, os
+from mysql.connector import errorcode
+import csv 
+from datetime import date
+import datetime
 
-# REFERENCES:
-# https://www.geeksforgeeks.org/implementing-web-scraping-python-beautiful-soup/
-
-# This is an HTML scraper that does the following:
-# 	1. Go to https://www.scholarships.com/financial-aid/college-scholarships/scholarship-directory/academic-major/computer-science
-# 	2. Scrape the first page of listings (<td> elements inside <tr>)
-# 	3. Output data as importable CSB (Outdated step: Turn the collected data into JSON)
-# 	4. Store the JSON to file, where it can be accessed by the Py-SQL script
-
-
-# TODO Delete duplicate scholarships from .csv file or decide to remove duplicates at Database level.
-
-import csv  # For writing data to .CSV file
-from datetime import date  # For titling the CSV file
-import html5lib  # For parsing HTML
-import requests  # For querying HTML from websites
-from time import sleep
-from bs4 import BeautifulSoup  # BeautifulSoup4, the parse tree module
-from os import environ
-from scrape import scrape, toAmount, toDate
+#re : regular expression
+import re                              
 
 
-def get_scholarshipscom_description(url, appendable_url):
-    """Get the description for a scholarship.
-    Args:
-        url (str): URL to scholarship page
-        appendable_url (str): base site url
-    Returns:
-        str: The scholarship description
-    """
-    response = get_response(appendable_url + url)
-    soup = BeautifulSoup(response.content, 'html5lib')
-
-    # Get description text
-    raw_desc = soup.find('li', attrs={'class': 'scholdescrip'}).get_text()
-
-    # Remove google ad code
-    raw_desc = raw_desc.replace('(adsbygoogle = window.adsbygoogle || []).push({});', '')
-
-    # Remove extra white spaece from front and back of string
-    raw_desc = raw_desc.strip()
-
-    return raw_desc
 
 
-def get_scholarshipscom_details(url, appendable_url, filename):
-    """Get the details from the academic major page and save them to a .csv file.
-    Args:
-            url (str): Url to scholarship page
-            appendable_url (str): Base site url
-            filename (str): .csv to save scholarships to
-    """
-    response = get_response(appendable_url + url)
-    soup = BeautifulSoup(response.content, 'html5lib')
-
-    print("Finding scholarships for: %s" % url, flush=True)
-
-    # Get scholarship table
-    scholarshipList = []
-    table = soup.find('tbody')
-
-    # Get elements from table
-    for row in table.findAll('tr'):
-        scholarship = {}
-        scholarship['name'] = row.find(
-            'td', attrs={'class': 'scholtitle'}).text
-        scholarship['url'] = appendable_url + str(row.a['href'])
-        scholarship['amount'] = toAmount(row.find(
-            'td', attrs={'class': 'scholamt'}).text)
-        scholarship['deadline'] = toDate(row.find(
-            'td', attrs={'class': 'scholdd'}).text)
-        scholarship['description'] = get_scholarshipscom_description(row.find('td', attrs={'class': 'scholtitle'}).a['href'], appendable_url)
-        scholarshipList.append(scholarship)
-
-    # print(scholarshipList)
-
-    # Write scholarships to file
-    with open(filename, 'a', encoding='utf-8-sig') as f:
-        w = csv.DictWriter(f, ['name', 'url', 'amount', 'deadline', 'description'])
-        for scholarship in scholarshipList:
-            w.writerow(scholarship)
-
-
-def get_response(url):
-    """Connect to website, stopping the program if response code not OK.
-    Args:
-            url (str): String url to get request
-    Returns:
-            response: Response object
-    """
+ 
+# scrape 
+# @param username, password, host IP or name 
+# feature : This functions take the csv file made by scrape.py. 
+#          It creates and inserts rows in the scholarship table. 
+#          It also makes the corresponding tags rows in the Reqtag table.
+#          They are linked by `Scholarship`.`idScholarship` and `Reqtag`.`idreqtag`
+def scrape(usern, passwd, hostl, databasen):
     try:
-        response = requests.get(url)
-    except requests.RequestException as e:
-        print(e)
-        exit(1)
+        conx = sql.connect(user=usern, password=passwd , host=hostl, database=databasen)
+        cursor = conx.cursor()
 
-    return response
+        time = date.today()
+        filename = 'scan_'+str(time)+'.csv'
+        reader = csv.reader(open(filename))
 
+        i = 0
+        for row in reader:
+            if i == 0:
+                i = i + 1
+                continue
+            else:  
+                # Creating and inserting the scholarships by row.
+                # The scholarships are missing the `desc` attribute. Otherwise all attributes are satisfied 
+                # 
+                # Scholarship table's attributes : idScholarship, desc, name, amount, deadline, idreqtag, url, accp_status
+                #
+                scholarshipQuery ="""
+                                INSERT INTO Scholarship (name, url, amount, deadline) VALUES (%s,%s,%s,%s)
+                                """
+                scholarshipData = (row[0], row[1], int(row[2]), row[3])
+                cursor.execute(scholarshipQuery, scholarshipData)
+                
 
-def main():
-    """Code for scraper."""
-    url = "https://www.scholarships.com/financial-aid/college-scholarships/scholarship-directory/academic-major"
-    # Use to make URL attribute of scholarship object usable
-    appendable_url = "https://www.scholarships.com"
-
-    # Setup output file
-    scan_time = date.today()
-    filename = 'scan_' + str(scan_time) + '.csv'
-    with open(filename, 'w', encoding='utf-8-sig') as f:
-        w = csv.DictWriter(f, ['name', 'url', 'amount', 'deadline', 'description'])
-        w.writeheader()
-
-    # get response
-    response = get_response(url)
-
-    soup = BeautifulSoup(response.content, 'html5lib')
-    url_table = soup.find(id="ullist")
-    url_list = url_table.find_all('a')
-    for link in url_list:
-        get_scholarshipscom_details(link.get('href'), appendable_url, filename)
-
-        # Wait 1 second between requests
-        sleep(1)
-
-    print("Pushing file into the database.", flush=True)
-    scrape(environ['MYSQL_USER'], environ['MYSQL_PASSWORD'], "db", environ['MYSQL_DB_NAME'])
-
-    print("done")
+                # Geting the last inserted scholarship's ID.
+                cursor.execute("SELECT idScholarship FROM Scholarship where idScholarship = (SELECT LAST_INSERT_ID())")
+                lastScholarshipID = cursor.fetchone()
 
 
-if __name__ == "__main__":
-    main()
+                # tagBuilder(idScholarship, url, desc) : Gives us an array of all attributes of the Reqtag table per scholarship listing
+                # Reqtag table's attributes : sex, major, citizenship, essay, GPA, ethnicity, idScholarship
+                #
+                s = tagBuilder(lastScholarshipID[0], row[1], " ")
+                tagQuery ="""
+                        INSERT INTO Reqtag (sex, major, citizenship, essay, GPA, ethnicity, idScholarship) VALUES (%s, %s, %s, %s, %s, %s, %s)
+                        """
+                tags = (s[0], s[1], s[2], s[3], s[4], s[5], s[6])
+                cursor.execute(tagQuery, tags)
+
+                # Geting the last inserted tag's ID.
+                cursor.execute("SELECT idreqtag FROM Reqtag where idreqtag = (SELECT LAST_INSERT_ID())")
+                lastReqtagID = cursor.fetchone()
+
+                # Linking the two relations together
+                cursor.execute("UPDATE Scholarship set idreqtag = %s where idScholarship = %s", (lastReqtagID[0], lastScholarshipID[0]))
+
+
+
+        # queries to NULL where deadline = '1000-01-01' and where amount = 0
+        cursor.execute("UPDATE Scholarship set deadline = NULL where deadline = '1000-01-01'")
+        cursor.execute("UPDATE Scholarship set amount = NULL where amount = 0")
+
+        # queries to NULL where GPA = '-1' and where ethnicity = '-1' and where sex = '-1'
+        cursor.execute("UPDATE Reqtag set GPA = NULL where GPA = '-1'")
+        cursor.execute("UPDATE Reqtag set ethnicity = NULL where ethnicity = '-1'")
+        cursor.execute("UPDATE Reqtag set sex = NULL where sex = '-1'")
+
+
+
+
+        print("Successful insertion of scraped scholarship - Scraped filename : %s" %filename)
+        # Close 
+        conx.commit()
+        cursor.close()
+        conx.close()
+    except sql.Error as er:
+        if er.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with username or password")
+        elif er.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exit")
+        else:
+            print(er)
+
+
+# This function formats the date from '%m/%d/%Y' to '%Y-%m-%d' so we can add it to MYSQL
+# @param : 
+#   string in the format of '%m/%d/%Y'
+def toDate(date_str):
+    
+    # Return the earliest date possible - This can't be NULL or empty here, so this is why I did this. 
+    if date_str == 'Varies' or date_str == 'varies' or date_str == 'deadline':
+        return '1000-01-01'
+    else:
+        format_str = '%m/%d/%Y'                                             # The old date format
+        datetime_obj = datetime.datetime.strptime(date_str, format_str)     # This will give use the formatted object which is then turned into a date string. 
+        return datetime_obj.date() 
+
+
+
+# This function formats the amount where if equals to 'varies' then return 0. It also takes out any spaces or ',' 
+# @param : string in the format of '%m/%d/%Y'
+def toAmount(amount):
+    if amount == 'Varies' or amount == 'varies':
+        return 0
+    else:
+        line = re.sub('[Variesv!,@#$ ]', '', amount)            # subtracting characters with regex
+        if line == '':
+            line = 0
+        return int(line)
+
+
+
+# This function builds a string to 
+# @param : 
+#   idscholarship - The ID of the scholarship that this table is linked to
+#   url - Full link of the site
+#   description - !! This is will be used when we get mor information
+def tagBuilder(idscholarship, url, desc):
+
+        # The array for all the attributes in the Reqtag table. As of right now there are
+        # Peusdo values
+        s = ['-1','','0','0','-1','-1',idscholarship]
+
+        # Getting the major from url link 
+        temp = url
+        front, back = temp.split('/academic-major/')
+        major, other = back.split('/')
+        s[1] = major
+        return s
+
