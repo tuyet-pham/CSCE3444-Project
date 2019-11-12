3611e1ffaff1c4eae593bd6d3424516c1a02c728
tuyet-pham <tiipham19@gmail.com>
2019-11-12 13:20:02 -0600

    fixed

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python
-#scholscrape_new.py -- optimized for Python v3.7.3.
+"""This is an HTML scraper that does the following.
+1. Go to https://www.scholarships.com/financial-aid/college-scholarships/scholarship-directory/academic-major/computer-science
+2. Scrape the first page of listings (<td> elements inside <tr>)
+3. Output data as importable CSB (Outdated step: Turn the collected data into JSON)
+4. Store the JSON to file, where it can be accessed by the Py-SQL script
+"""
+# scholscrape_new.py -- optimized for Python v3.7.3.
 
-#CONTRIBUTORS: Peyton Pritchard, Joe Maggio
+# CONTRIBUTORS: Peyton Pritchard, Joe Maggio
 
-#REFERENCES:
+# REFERENCES:
 # https://www.geeksforgeeks.org/implementing-web-scraping-python-beautiful-soup/
 
 # This is an HTML scraper that does the following:
 # 	1. Go to https://www.scholarships.com/financial-aid/college-scholarships/scholarship-directory/academic-major/computer-science
-#	2. Scrape the first page of listings (<td> elements inside <tr>)
-#	3. Output data as importable CSB (Outdated step: Turn the collected data into JSON)
-#	4. Store the JSON to file, where it can be accessed by the Py-SQL script
+# 	2. Scrape the first page of listings (<td> elements inside <tr>)
+# 	3. Output data as importable CSB (Outdated step: Turn the collected data into JSON)
+# 	4. Store the JSON to file, where it can be accessed by the Py-SQL script
 
 
-#TODO Delete duplicate scholarships from .csv file or decide to remove duplicates at Database level.
+# TODO Delete duplicate scholarships from .csv file or decide to remove duplicates at Database level.
 
 import csv  # For writing data to .CSV file
 from datetime import date  # For titling the CSV file
@@ -21,91 +27,88 @@ import html5lib  # For parsing HTML
 import requests  # For querying HTML from websites
 from time import sleep
 from bs4 import BeautifulSoup  # BeautifulSoup4, the parse tree module
-from scrape import *
+from os import environ
+from scrape import scrape, toAmount, toDate
+
+
+def get_scholarshipscom_description(url, appendable_url):
+    """Get the description for a scholarship.
+    Args:
+        url (str): URL to scholarship page
+        appendable_url (str): base site url
+    Returns:
+        str: The scholarship description
+    """
+    response = get_response(appendable_url + url)
+    soup = BeautifulSoup(response.content, 'html5lib')
+
+    # Get description text
+    raw_desc = soup.find('li', attrs={'class': 'scholdescrip'}).get_text()
+
+    # Remove google ad code
+    raw_desc = raw_desc.replace('(adsbygoogle = window.adsbygoogle || []).push({});', '')
+
+    # Remove extra white spaece from front and back of string
+    raw_desc = raw_desc.strip()
+
+    return raw_desc
 
 
 def get_scholarshipscom_details(url, appendable_url, filename):
-	"""Get the details from the academic major page and save them to a .csv file.
-	Args:
-		url (str): Url to scholarship page
-		appendable_url (str): Base site url
-		filename (str): .csv to save scholarships to
-	"""
-	response = get_response(appendable_url + url)
-	soup = BeautifulSoup(response.content, 'html5lib')
+    """Get the details from the academic major page and save them to a .csv file.
+    Args:
+            url (str): Url to scholarship page
+            appendable_url (str): Base site url
+            filename (str): .csv to save scholarships to
+    """
+    response = get_response(appendable_url + url)
+    soup = BeautifulSoup(response.content, 'html5lib')
 
-	# Get scholarship table
-	scholarshipList = []
-	table = soup.find('tbody')
+    print("Finding scholarships for: %s" % url, flush=True)
 
-	# Get elements from table
-	for row in table.findAll('tr'):
-		scholarship = {}
-		scholarship['name'] = row.find('td', attrs = {'class':'scholtitle'}).text
-		scholarship['url'] = appendable_url+str(row.a['href'])
-		scholarship['amount'] = toAmount(row.find('td', attrs = {'class':'scholamt'}).text)			# Using the func toAmount from scrape.py to format
-		scholarship['deadline'] = toDate(row.find('td', attrs = {'class':'scholdd'}).text)			# Using the func toDate from scrape.py to format
-		scholarshipList.append(scholarship)
+    # Get scholarship table
+    scholarshipList = []
+    table = soup.find('tbody')
 
-	# print(scholarshipList)
+    # Get elements from table
+    for row in table.findAll('tr'):
+        scholarship = {}
+        scholarship['name'] = row.find(
+            'td', attrs={'class': 'scholtitle'}).text
+        scholarship['url'] = appendable_url + str(row.a['href'])
+        scholarship['amount'] = toAmount(row.find(
+            'td', attrs={'class': 'scholamt'}).text)
+        scholarship['deadline'] = toDate(row.find(
+            'td', attrs={'class': 'scholdd'}).text)
+        scholarship['description'] = get_scholarshipscom_description(row.find('td', attrs={'class': 'scholtitle'}).a['href'], appendable_url)
+        scholarshipList.append(scholarship)
 
-	# Write scholarships to file
-	with open(filename, 'a') as f:
-		w = csv.DictWriter(f, ['name','url','amount','deadline'])
-		for scholarship in scholarshipList:
-			w.writerow(scholarship)
+    # print(scholarshipList)
 
+    # Write scholarships to file
+    with open(filename, 'a', encoding='utf-8-sig') as f:
+        w = csv.DictWriter(f, ['name', 'url', 'amount', 'deadline', 'description'])
+        for scholarship in scholarshipList:
+            w.writerow(scholarship)
 
 
 def get_response(url):
-	"""Connect to website, stopping the program if response code not OK
+    """Connect to website, stopping the program if response code not OK.
+    Args:
+            url (str): String url to get request
+    Returns:
+            response: Response object
+    """
+    try:
+        response = requests.get(url)
+    except requests.RequestException as e:
+        print(e)
+        exit(1)
 
-	Args:
-		url (str): String url to get request
-
-	Returns:
-		response: Response object
-	"""
-	try:
-		response = requests.get(url)
-	except exceptions.RequestException as e:
-		print(e)
-		sys.exit(1)
-
-	return response
+    return response
 
 
 def main():
-	"""Main code for scraper
-	"""
-	url = "https://www.scholarships.com/financial-aid/college-scholarships/scholarship-directory/academic-major"
-	appendable_url = "https://www.scholarships.com" # Use to make URL attribute of scholarship object usable
-
-	# Setup output file
-	scan_time = date.today()
-	filename = 'scan_'+str(scan_time)+'.csv'
-	with open(filename, 'w') as f:
-		w = csv.DictWriter(f, ['name','url','amount','deadline'])
-		w.writeheader()
-
-	# get response
-	response = get_response(url)
-
-	soup = BeautifulSoup(response.content, 'html5lib')
-	url_table = soup.find(id="ullist")
-	url_list = url_table.find_all('a')
-	for link in url_list:
-		get_scholarshipscom_details(link.get('href'), appendable_url, filename)
-		# Wait 1 second between requests
-		sleep(1)
-
-	# scrape("","","","")				#inserting after the scraper is finished
-	print("Done\nFilename : " %filename)
-
-if __name__== "__main__":
-  main()
-
-=======
     """Code for scraper."""
     url = "https://www.scholarships.com/financial-aid/college-scholarships/scholarship-directory/academic-major"
     # Use to make URL attribute of scholarship object usable
@@ -114,8 +117,8 @@ if __name__== "__main__":
     # Setup output file
     scan_time = date.today()
     filename = 'scan_' + str(scan_time) + '.csv'
-    with open(filename, 'w',encoding='utf-8-sig') as f:
-        w = csv.DictWriter(f, ['name', 'url', 'amount', 'deadline'])
+    with open(filename, 'w', encoding='utf-8-sig') as f:
+        w = csv.DictWriter(f, ['name', 'url', 'amount', 'deadline', 'description'])
         w.writeheader()
 
     # get response
@@ -126,10 +129,13 @@ if __name__== "__main__":
     url_list = url_table.find_all('a')
     for link in url_list:
         get_scholarshipscom_details(link.get('href'), appendable_url, filename)
+
         # Wait 1 second between requests
         sleep(1)
 
+    print("Pushing file into the database.", flush=True)
     scrape(environ['MYSQL_USER'], environ['MYSQL_PASSWORD'], "db", environ['MYSQL_DB_NAME'])
+
     print("done")
 
 
