48ff480ebd8dad0c3abbaea10d356bd0d08789a1
tuyet-pham <tiipham19@gmail.com>
2019-11-21 05:16:11 -0600

    ss

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -15,11 +15,7 @@ import Submit from './Submit';
 import {AiFillHome} from "react-icons/ai";
 import {FaSearch} from "react-icons/fa";
 import {MdAddCircle} from "react-icons/md"
-<<<<<<< HEAD
 import {FaUserCircle} from "react-icons/fa"
-=======
-//import {IoMdLogIn} from "react-icons/iomd"
->>>>>>> 8d42379f7afb533b21cf9095571154f4b60021c8
   
 
 class App extends React.Component {
@@ -30,7 +26,6 @@ class App extends React.Component {
         }
     }
 
-    // https://hackernoon.com/passing-arguments-to-react-event-handlers-the-easy-way-3bf8e52f7705
     navClick (e) {
         if(e == 0){
             this.setState({
@@ -53,20 +48,12 @@ class App extends React.Component {
         return (
             <div className="App">
                 <Router>
-<<<<<<< HEAD
                     <div style={{backgroundColor: this.state.bgColor}} class="nav2">
                         <Link onClick={this.navClick.bind(this,1)} title="Home" to ="/"><AiFillHome/></Link>
                         <Link onClick={this.navClick.bind(this,1)} title="Search for scholarships" to ="/search"><FaSearch/></Link>
                         <Link onClick={this.navClick.bind(this,0)} title="Add a listing" to ="/submit"><MdAddCircle/></Link>
                         <Link onClick={this.navClick.bind(this,2)} title="Admin Login" to ="/login"><FaUserCircle/></Link>
                         {/* <Link title="Admin Home" to ="/adhome">AdminHome</Link> */}
-=======
-                    <div class="nav2">
-                        <Link title="Home" to ="/root"><FaHome/></Link>
-                        <Link title="Search for scholarships" to ="/search"><FaSearch/></Link>
-                        <Link title="Add a listing" to ="/submit"><MdAddCircle/></Link>
-                        <Link title="Admin Login" to ="/login">Login</Link>
->>>>>>> 8d42379f7afb533b21cf9095571154f4b60021c8
                     </div>
                     <Switch>
                         <Route path="/adhome">
