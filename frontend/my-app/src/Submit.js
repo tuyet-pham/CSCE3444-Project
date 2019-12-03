6c36adbfdb2d152c15555a8503728d68d6f5bf01
Pedro Sanchez <chablo2499@gmail.com>
2019-12-03 17:32:07 -0600

    Created validation for GPA and amount

--- frontend/my-app/src/Submit.js
+++ frontend/my-app/src/Submit.js
@@ -8,7 +8,6 @@ class Submit extends React.Component {
         super();
         var x = new Date();
         console.log(x);
-        this.handleSubmit = this.handleSubmit.bind(this);
         this.state = {
             name: "",
             url: "",
@@ -17,24 +16,34 @@ class Submit extends React.Component {
             date: x.get,
             deadline: "",
             ethnicity: "",
-            sex: -1,
-            citizenship: -1,
-            essay: -1,
+            sex: "",
+            citizenship: "",
+            essay: "",
             major: "",
             description: "",
             accp_status: -1
         };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e)
+    {
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name] : value
+        });
     }
 
     handleSubmit = (e) => {
-        const errormsg = "Your input is invalid, try again";
-        const goodmsg = "";
+        const goodmsg = "Thank you for your submission!";
         
-        if(!this.validate(e))
+        e.preventDefault(); //Don't allow page to reload
+        if(this.validate(e))
         {
-            alert(errormsg);
-        }
-        else {
             alert(goodmsg);
         }
 
@@ -44,8 +53,19 @@ class Submit extends React.Component {
         //delete
     }
 
-    validate( ) {
-        //if value is good then return good alert, if not then alert bad. 
+    validate(e) {
+        if(this.state.amount === parseInt(this.state.amount, 10) || this.state.amount <= 0)
+        {
+            alert("Amount not valid.");
+            return false;
+        }
+        else if(this.state.GPA === parseInt(this.state.GPA, 10) || this.state.GPA <= 0.0 || this.state.GPA > 4.0)
+        {
+            alert("GPA not valid.");
+            return false;
+        }
+       
+        return true;
     }
 
     render () {
@@ -58,39 +78,39 @@ class Submit extends React.Component {
                         <form onSubmit={this.handleSubmit}>
                             <div class="row2">
                                 <div class="column">
-                                    <input type="text" max="300" name="name" class="admininput2" placeholder="scholarship name" required/><strong><abbr title="required">*</abbr></strong>
-                                    <input type="url"  max="300" name="url" class="admininput2" placeholder="scholarship url" required/><strong><abbr title="required">*</abbr></strong>
+                                    <input type="text" max="300" name="name" class="admininput2" placeholder="scholarship name" value={this.state.value} onChange={this.handleChange} required/><strong><abbr title="required">*</abbr></strong>
+                                    <input type="url"  max="300" name="url" class="admininput2" placeholder="scholarship url" value={this.state.value} onChange={this.handleChange} required/><strong><abbr title="required">*</abbr></strong>
                                 </div>
                                 <div class="column">
-                                    <input type="text" style={{width:"50%"}} name="amount" class="admininput2" placeholder="amount"/>
-                                    <input type="amount"  max="4.0" style={{width:"50%"}} name="GPA" class="admininput2" placeholder="Minimum GPA" required/><strong><abbr title="required">*</abbr></strong>
+                                    <input type="text" style={{width:"50%"}} name="amount" class="admininput2" placeholder="amount" value={this.state.value} onChange={this.handleChange}/>
+                                    <input type="amount"  max="4.0" style={{width:"50%"}} name="GPA" class="admininput2" placeholder="Minimum GPA" value={this.state.value} onChange={this.handleChange} required/><strong><abbr title="required">*</abbr></strong>
                                 </div>
                                 <div class="column">
                                     Ethnicity<br/>
-                                    <select style={{width:"100%"}} id="ethnicity" name="ethnicity">
-                                        <option value="hispanic">Black</option>
-                                        <option value="hispanic">Asian</option>
-                                        <option value="hispanic">Hispanic</option>
-                                        <option value="hispanic">Native American</option>
-                                        <option value="hispanic">Caucasian</option>
-                                        <option value="hispanic">Pacific Islander</option>
-                                        <option value="hispanic">Other</option>
+                                    <select style={{width:"100%"}} id="ethnicity" name="ethnicity" value={this.state.value} onChange={this.handleChange}>
+                                        <option value="Black">Black</option>
+                                        <option value="Asian">Asian</option>
+                                        <option value="Hispanic">Hispanic</option>
+                                        <option value="Native American">Native American</option>
+                                        <option value="Caucasian">Caucasian</option>
+                                        <option value="Pacific Islander">Pacific Islander</option>
+                                        <option value="">Other</option>
                                     </select>
                                 </div>
                                 <div class="column">
                                     Deadline <br/>
-                                    <input type="date" style={{width:"50%"}} name="deadline" class="admininput2" required/><strong><abbr title="required">*</abbr></strong>
+                                    <input type="date" style={{width:"50%"}} name="deadline" class="admininput2" value={this.state.value} onChange={this.handleChange} required/><strong><abbr title="required">*</abbr></strong>
                                 </div>
                             </div> 
                             <div class="row2">
                                 <div class="column">
                                     Gender <br/>
-                                    <input type="radio" id="0" name="gender" value="F" /> Female <br/>
-                                    <input type="radio" id="1" name="gender" value="M" /> Male <br/>
-                                    <input type="radio" id="-1" name="gender" value="F" /> Varies
+                                    <input type="radio" name="sex" value="1" onChange={this.handleChange}/> Female <br/>
+                                    <input type="radio" name="sex" value="2" onChange={this.handleChange}/> Male <br/>
+                                    <input type="radio" name="sex" value="-1" onChange={this.handleChange}/> Varies
                                     <br/><br/><br/><br/>
                                     Academic Major <strong><abbr title="required">*</abbr></strong><br/>
-                                    <select style={{width:"100%"}} id="major" name="Major">
+                                    <select style={{width:"100%"}} name="major" value={this.state.value} onChange={this.handleChange}>
                                         <option value="academic" required>Academics</option>
                                         <option value="aerospace">Aerospace</option>
                                         <option value="agriculture">Agriculture</option>
@@ -103,12 +123,12 @@ class Submit extends React.Component {
                                 </div>
                                 <div class="column">
                                     Requires citizenship? <strong><abbr title="required">*</abbr></strong><br/> 
-                                    <input type="radio" id="0" name="citizenship" value="0" required/> Y <br/>
-                                    <input type="radio" id="1" name="citizenship" value="1" /> N 
+                                    <input type="radio" name="citizenship" value="1" onChange={this.handleChange} required/> Y <br/>
+                                    <input type="radio" name="citizenship" value="0" onChange={this.handleChange}/> N 
                                     <br/><br/>
                                     Is there an essay involved? <br/>
-                                    <input type="radio" id="0" name="essay" value="0" /> Y <br/>
-                                    <input type="radio" id="1" name="essay" value="1" /> N
+                                    <input type="radio" name="essay" value="1" onChange={this.handleChange}/> Y <br/>
+                                    <input type="radio" name="essay" value="0" onChange={this.handleChange}/> N
                                 </div>
                             </div> 
                             <div class="row2">
