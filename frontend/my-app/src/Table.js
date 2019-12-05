d2c6f1d448fe538fcc16235a8b81b3b7d5b60fe8
tuyet-pham <tiipham19@gmail.com>
2019-12-05 01:39:55 -0600

    + Linking Table conponents

--- frontend/my-app/src/Table.js
+++ frontend/my-app/src/Table.js
@@ -9,25 +9,9 @@ class Table extends React.Component {
     }
 
     render (){
-        const scholarshipData = {
-            row: [
-                {id: this.props.id},
-                {name: this.props.name},
-                {description: this.props.description}
-
-            ]
-        }
-
         return (
             <div>
-                Prop test : {this.props.message}      
-               <table>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Description</th>
-                    </tr>
-               </table>
+               
             </div>
         )
     }
