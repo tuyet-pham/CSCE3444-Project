c2f8cd86a3b5067a6e37380268b72a4083912634
Peyton Pritchard <pritchardpeyton119@gmail.com>
2019-12-06 17:30:05 -0600

    Removed search bar from home page.

--- frontend/my-app/src/Home.js
+++ frontend/my-app/src/Home.js
@@ -20,10 +20,6 @@ class Home extends React.Component {
                         <img class="App-logo" src={process.env.PUBLIC_URL + "scraper_logo.png"} alt="ScholarScraper logo"/> 
                         ScholarScraper 
                     </h1>
-                    <div class="Search-bar">
-                        <input type="text" placeholder="Search scholarships..." name="search" />
-                        <button type="submit">Go</button>
-                    </div>
                 </div>
 
                 {/*put scraper tips here*/}
